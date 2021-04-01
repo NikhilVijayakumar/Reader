69fc3278dde9e69051e5839b39d006c89f0e4ed7
Nikhil Vijayakumar <NikhilVijayakumar@users.noreply.github.com>
2021-04-01 16:32:32 +0530

    PDF  stock file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from pdfminer import high_level
 import pyttsx3
-import os
-
+from pdfminer.high_level import extract_pages
 
 engine = pyttsx3.init()
 engine.setProperty('rate', 150)
@@ -9,19 +8,12 @@ engine.setProperty('volume',1.2)
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 local_pdf_filename = "pdf/Stock.pdf"
-pages = [0]
-extracted_text = high_level.extract_text(local_pdf_filename, "", pages)
-print(extracted_text)
-# with  open("1.txt", "r",encoding="utf8") as f:
-#     data = f.read()
-# engine.say(extracted_text)
-# engine.runAndWait()
-# engine.stop()
-
-# fullPath = os.path.join(os.getcwd(), 'test.wav')
-# print(fullPath)
-# engine.save_to_file(extracted_text,fullPath)
-# engine.runAndWait()
-engine.say(extracted_text)
-engine.save_to_file('Hello World' , 'test.mp3')
-engine.runAndWait()
+pages = [1]
+pageNumber = len(list(extract_pages(local_pdf_filename)))
+print(pageNumber)
+for page in range(7,pageNumber):
+    pages = [page]
+    extracted_text = high_level.extract_text(local_pdf_filename, "", pages)
+    print(extracted_text)
+    engine.say(extracted_text)
+    engine.runAndWait()
