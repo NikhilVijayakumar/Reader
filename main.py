5e176d4d90d9fe268c6fb15dfef3dbae7fb63ce2
Nikhil Vijayakumar <NikhilVijayakumar@users.noreply.github.com>
2021-04-03 19:24:03 +0530

    PDF  stock file updated

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ engine.setProperty('rate', 150)
 engine.setProperty('volume',1.2)
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-local_pdf_filename = "pdf/Stock.pdf"
+local_pdf_filename = "pdf/Stock_3_4_2021.pdf"
 pages = [1]
 pageNumber = len(list(extract_pages(local_pdf_filename)))
 print(pageNumber)
-for page in range(7,pageNumber):
+for page in range(6,pageNumber+1):
     pages = [page]
     extracted_text = high_level.extract_text(local_pdf_filename, "", pages)
     print(extracted_text)
     engine.say(extracted_text)
     engine.runAndWait()
+engine.stop()
